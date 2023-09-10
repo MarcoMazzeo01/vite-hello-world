87702cf405ce729aefcfedafd80985a1f66032c9
plastikSun <marcom2k01@gmail.com>
2023-09-10 19:08:28 +0200

    add: app title

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {
+      msg: "La mia prima app in Vite!",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<style scoped></style>

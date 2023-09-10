9ff93af049b4e3da931056c5cf98ff59aee4e3ae
plastikSun <marcom2k01@gmail.com>
2023-09-10 22:50:45 +0200

    add: jumbotron (bonus)

---
+++ src/components/AppHero.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  data() {
+    return {
+      img: new URL(`..\assets\jumbotron.jpg`, import.meta.url).href,
+    };
+  },
+  mounted() {
+    const myDiv = document.getElementById("jumbotron");
+    myDiv.style.setProperty("--background-image", `url(${this.img})`);
+  },
+};
+</script>
+
+<template>
+  <div class="container-fluid" id="jumbotron">
+    <!-- <img :src="img" alt="jumbotron" /> -->
+  </div>
+</template>
+
+<style>
+#jumbotron {
+  height: 200px;
+  background-image: var(--background-image);
+  background-repeat: repeat-x;
+}
+</style>
